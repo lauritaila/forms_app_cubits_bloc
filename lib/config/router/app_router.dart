a540d837c84c4459c282db7b06cf2cd0dd0e8e98
Laura Avila <lavila@bigwise.com>
2025-04-14 14:56:18 -0400

    FEAT: app register screen router

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -6,4 +6,5 @@ final appRouter = GoRouter(routes: [
   GoRoute(path: '/', builder: (context, state) => const HomeScreen()),
   GoRoute(path: '/cubits', builder: (context, state) => const CubitCounterScreen()),
   GoRoute(path: '/bloc', builder: (context, state) => const BlocCounterScreen()),
+  GoRoute(path: '/new-account', builder: (context, state) => const RegisterScreen()),
 ]);
