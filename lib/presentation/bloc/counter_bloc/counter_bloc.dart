207bcb2fe922dce7df7eaecc05797e5e1ffe847d
Laura Avila <lavila@bigwise.com>
2025-04-14 09:49:16 -0400

    FEAT: counter bloc

---
+++ lib/presentation/bloc/counter_bloc/counter_bloc.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'counter_event.dart';
+part 'counter_state.dart';
+
+class CounterBloc extends Bloc<CounterEvent, CounterState> {
+  CounterBloc() : super(CounterState()) {
+    on<CounterIncreased>(_onCounterIncreased);
+    on<CounterReset>(_onCounterReset);
+  }
+
+  void _onCounterIncreased(CounterIncreased event, Emitter<CounterState> emit) => emit(state.copyWith(counter: state.counter + event.value, transactionCount: state.transactionCount + 1));
+  void _onCounterReset(CounterReset event, Emitter<CounterState> emit) => emit(state.copyWith(counter: 0)); 
+
+  void increaseBy([int value = 0]) => add(CounterIncreased(value));
+  void reset() => add(CounterReset());
+}
