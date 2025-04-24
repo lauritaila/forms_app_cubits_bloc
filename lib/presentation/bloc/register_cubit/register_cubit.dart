a785da9e5318e38d87ad6111527f1ab5b00af621
Laura Avila <lavila@bigwise.com>
2025-04-24 16:41:00 -0400

    FEAT: validating data on submit

--- lib/presentation/bloc/register_cubit/register_cubit.dart
+++ lib/presentation/bloc/register_cubit/register_cubit.dart
@@ -9,6 +9,15 @@ class RegisterCubit extends Cubit<RegisterFormState> {
   RegisterCubit() : super(const RegisterFormState());
 
   void onSubmit() {
+    emit(
+      state.copyWith(
+        formStatus: FormStatus.validating,
+        username: Username.dirty(state.username.value),
+        password: Password.dirty(state.password.value),
+        isValid: Formz.validate([state.username, state.password]),
+      ),
+    );
+
     print('Submit: $state');
   }
 
@@ -19,14 +28,20 @@ class RegisterCubit extends Cubit<RegisterFormState> {
   void onUsernameChanged(String value) {
     final username = Username.dirty(value);
     emit(
-      state.copyWith(username: username, isValid: Formz.validate([username, state.password])),
+      state.copyWith(
+        username: username,
+        isValid: Formz.validate([username, state.password]),
+      ),
     );
   }
 
   void onPasswordChanged(String value) {
     final password = Password.dirty(value);
     emit(
-      state.copyWith(password: password, isValid: Formz.validate([password, state.username])),
+      state.copyWith(
+        password: password,
+        isValid: Formz.validate([password, state.username]),
+      ),
     );
   }
 }
