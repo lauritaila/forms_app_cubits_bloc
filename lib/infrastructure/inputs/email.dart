db252c6cd4fee63a00dac8e91b69f31f7bdc2238
Laura Avila <lavila@bigwise.com>
2025-04-24 16:49:56 -0400

    FEAT:  email imput

---
+++ lib/infrastructure/inputs/email.dart
@@ -0,0 +1,29 @@
+import 'package:formz/formz.dart';
+
+enum EmailInputError { empty, format }
+
+class Email extends FormzInput<String, EmailInputError> {
+  static final emailRegExp = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+  const Email.pure() : super.pure('');
+
+  const Email.dirty(super.value) : super.dirty();
+
+
+  String? get errorMessage {
+    if(isValid || isPure) return null;
+
+    if (displayError == EmailInputError.empty) return 'Email is required';
+    if (displayError == EmailInputError.format) return 'Invalid email';
+
+    return null;
+  }
+
+  @override
+  EmailInputError? validator(String value) {
+
+    if (value.isEmpty || value.trim().isEmpty)  EmailInputError.empty;
+    if (!emailRegExp.hasMatch(value))  EmailInputError.format;
+    
+    return null;
+  }
+}
